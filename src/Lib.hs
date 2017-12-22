6a6afc3a16fdbbb1c888b3c81d95d5514ebfc76f
hrb90 <harrisonrbrown90@gmail.com>
2017-12-21 23:19:31 -0500

    hindent

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,25 +11,27 @@ data Expr
   | Product [Expr]
 
 testExpr :: Expr
-testExpr = Sum [ Literal 300
-               , Product [ Literal 10
-                         , Literal 5
-                         , Literal 1
-                         , Literal 10
-                         , Literal 2 ]
-               , Product [ Literal 2
-                         , Literal 2
-                         , Sum [ Literal 1, Literal 1 ]
-                         , Literal 2
-                         , Literal 2
-                         , Literal 2 ]
-                , Literal 200
-               ]
+testExpr =
+  Sum
+    [ Literal 300
+    , Product [Literal 10, Literal 5, Literal 1, Literal 10, Literal 2]
+    , Product
+        [ Literal 2
+        , Literal 2
+        , Sum [Literal 1, Literal 1]
+        , Literal 2
+        , Literal 2
+        , Literal 2
+        ]
+    , Literal 200
+    ]
 
 parens' :: [Doc ann] -> [Doc ann]
 parens' xs = [pretty "("] `mappend` xs `mappend` [pretty ")"]
 
 instance Pretty Expr where
   pretty (Literal x) = pretty x
-  pretty (Sum xs) = align $ sep $ parens' $ punctuate (space <> pretty "+") (fmap pretty xs)
-  pretty (Product xs) = align $ sep $ punctuate (space <> pretty "*") (fmap pretty xs)
+  pretty (Sum xs) =
+    align $ sep $ parens' $ punctuate (space <> pretty "+") (fmap pretty xs)
+  pretty (Product xs) =
+    align $ sep $ punctuate (space <> pretty "*") (fmap pretty xs)
